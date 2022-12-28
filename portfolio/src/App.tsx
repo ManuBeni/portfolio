5627e6ba0039c548e3267edbf9743154bc82a9d7
root <root@mxnu>
2022-12-28 13:34:45 -0300

    working background particles

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Particle from './components/Particle';
 
 function App() {
   return (
     <div className="App">
+      <Particle/>
+      <div>
       <h1>Hello I'm the portfolio</h1>
+      </div>
+      
     </div>
   );
 }
