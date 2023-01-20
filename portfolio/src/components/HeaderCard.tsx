ce7fec7f4717e29a3c097e1918f0696d8e60aec4
root <root@mxnu>
2023-01-20 10:40:38 -0300

    Projects window

--- portfolio/src/components/HeaderCard.tsx
+++ portfolio/src/components/HeaderCard.tsx
@@ -1,128 +1,130 @@
-import { 
-        Card, 
-        Stack, 
-        Image, 
-        Heading, 
-        Button, 
-        CardBody, 
-        CardFooter,
-        Text,
-        useColorModeValue,
-        useDisclosure,
-        Collapse,
-        Box
-        
-        } from '@chakra-ui/react'
-import styles from "./HeaderCard.module.css"
-import ProfilePic from "../resources/ProfilePic.png"
-import ThemeButton from './ThemeButton'
+import {
+  Card,
+  Stack,
+  Image,
+  Heading,
+  Button,
+  CardBody,
+  CardFooter,
+  Text,
+  useColorModeValue,
+  useDisclosure,
+  Collapse,
+  Box,
+} from "@chakra-ui/react";
+import styles from "./HeaderCard.module.css";
+import ProfilePic from "../resources/ProfilePic.png";
+import ThemeButton from "./ThemeButton";
+import ProjectsSection from "./ProjectsSection";
+import { motion } from "framer-motion";
 
 const HeaderCard = () => {
-
-  const color = useColorModeValue('gray.800','white' )  
-  const { isOpen, onToggle } = useDisclosure()
-  const onButtonClick = ()=>{
-      // using Java Script method to get PDF file
-      fetch('Manuel-Benitez-CV-PDF.pdf').then(response => {
-      response.blob().then(blob => {
-      // Creating new object of PDF file
-      const fileURL = window.URL.createObjectURL(blob);
-      // Setting various property values
-      let alink = document.createElement('a');
-      alink.href = fileURL;
-      alink.download = 'Manuel-Benitez-CV-PDF.pdf';
-      alink.click();
-      })
-      })
-      }
-    return (
-      <> 
+  const color = useColorModeValue("gray.800", "white");
+  const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: false });
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch("Manuel-Benitez-CV-PDF.pdf").then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "Manuel-Benitez-CV-PDF.pdf";
+        alink.click();
+      });
+    });
+  };
+  return (
+    <>
       <div className={styles.themeButton}>
-      <ThemeButton/>
+        <ThemeButton />
       </div>
-      <div className={styles.body}>
+      
+      <motion.div
+        className={styles.body}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
         <Card
-          direction={{ base: 'column', sm: 'row' }}
-          overflow='hidden'
-          variant='outline'
-          sx={{  
-          color:"white",
-          backdropFilter: "blur(6px)",
-          textAlign:"center",
-          webkitBackdropFilter : "blur(10px)",
-          backgroundColor: "rgba(255, 255, 255, 0.1)",
-          maxWidth: "100%",
-          maxHeight:"50%",
-          
+          direction={{ base: "column", sm: "row" }}
+          overflow="hidden"
+          variant="outline"
+          sx={{
+            color: "white",
+            backdropFilter: "blur(6px)",
+            textAlign: "center",
+            webkitBackdropFilter: "blur(10px)",
+            backgroundColor: "rgba(255, 255, 255, 0.1)",
+            maxWidth: "100%",
+            maxHeight: "50%",
           }}
         >
-          
           <Image
-            objectFit='cover'
-            maxW={{ base: '100%', sm: '300px' }}
-
+            objectFit="cover"
+            maxW={{ base: "100%", sm: "300px" }}
+            sx={{ borderBottomRightRadius: "15:%" }}
             src={ProfilePic}
-            alt='Caffe Latte'
+            alt="Caffe Latte"
           />
 
           <Stack>
-            
             <CardBody color={color}>
-              <Heading 
+              <Heading
                 ml="8px"
                 mr="8px"
-                size='2xl' 
+                size="2xl"
                 py="5"
-                // bgGradient='linear(to-l, #E07A5F, #F2CC8F)'
-                // bgClip='text'
-                fontSize='6xl'
-                fontWeight='700'
-                fontFamily='Syne'>
+                fontSize="4rem"
+                fontWeight="700"
+                fontFamily="Syne"
+              >
                 Hi! I'm Manuel Benitez
               </Heading>
-              <Text py='3' fontSize={28}>
+
+              <Text py="3" fontSize={28}>
                 Full Stack Developer - Front End Designer
               </Text>
-              <Text py='3' fontSize={20} wordBreak='break-all'>
-                React, Node, Express, Redux, JS, Typescript, <br/>
+
+              <Text py="3" fontSize={20} wordBreak="break-all">
+                React, Node, Express, Redux, JS, Typescript, <br />
                 Material UI, CSS, Sass, HTML, PostgresSQL, Figma
               </Text>
             </CardBody>
 
-            <CardFooter sx={{ justifyContent:"center"}}>
-              <Button variant='solid' color= {color} size="xlg" sx={{ bottom:"10px", padding:"2rem"}}
-              onClick={onButtonClick}>
-                Download CV
+            <CardFooter sx={{ justifyContent: "center" }}>
+              <motion.div
+                animate={{ rotate: [0, 0, -20, 20, 0] }}
+                transition={{ duration: 1.8 }}
+              >
+                <Button
+                  variant="solid"
+                  color={color}
+                  size="xlg"
+                  sx={{
+                    bottom: "10px",
+                    padding: "2rem",
+                  }}
+                  onClick={onButtonClick}
+                >
+                  Download CV
+                </Button>
+              </motion.div>
+              <Button ml="20px" mt="10px" onClick={onToggle} color={color}>
+                {isOpen ? "Hide Projects" : "Show Projects"}
               </Button>
-              <Button ml="20px" mt="10px" onClick={onToggle}>Projects</Button>
             </CardFooter>
           </Stack>
         </Card>
-        </div>
-        <div>
+      </motion.div>
+      <div>
         <Collapse in={isOpen} animateOpacity>
-            <Box
-          p='40px'
-          color='white'
-          mt='4'
-          bg='teal.500'
-          rounded='md'
-          shadow='md'
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo at iste, error accusantium, eveniet dolorem totam voluptatem eius a sequi distinctio vitae itaque perferendis ipsam nemo molestias quis quas?
-        
+          <ProjectsSection />
+        </Collapse>
+      </div>
       
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo at iste, error accusantium, eveniet dolorem totam voluptatem eius a sequi distinctio vitae itaque perferendis ipsam nemo molestias quis quas?
+    </>
+  );
+};
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo at iste, error accusantium, eveniet dolorem totam voluptatem eius a sequi distinctio vitae itaque perferendis ipsam nemo molestias quis quas?
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo at iste, error accusantium, eveniet dolorem totam voluptatem eius a sequi distinctio vitae itaque perferendis ipsam nemo molestias quis quas?
-
-        </Box>
-      </Collapse>
-        </div>
-        </>
-    )
-}
-
-export default HeaderCard
+export default HeaderCard;
--- portfolio/src/components/Particle.tsx
+++ portfolio/src/components/Particle.tsx
@@ -1,4 +1,3 @@
-
 import { useCallback } from "react";
 import type { Container, Engine } from "tsparticles-engine";
 import Particles from "react-particles";
@@ -7,194 +6,193 @@ import { useColorModeValue } from "@chakra-ui/react";
 
 const Particle = () => {
 
-    let toggle: boolean = false
+  let toggle: boolean = false;
 
-    const color = useColorModeValue('gray.800','white' )
-   
-    if(color==="white") {toggle = true} 
-    if (color === "gray.800") toggle = false
-    
-    const particlesInit = useCallback(async (engine: Engine) => {
-        console.log(engine);
+  const color = useColorModeValue("gray.800", "white");
 
-        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        await loadFull(engine);
-    }, []);
+  if (color === "white") {
+    toggle = true;
+  }
+  if (color === "gray.800") toggle = false;
 
-    const particlesLoaded = useCallback(async (container: Container | undefined) => {
-        console.log(container);
-    }, []);
-    return (
-                toggle ? 
-                <Particles
-                    id="tsparticles"
-                    init={particlesInit}
-                    loaded={particlesLoaded}
-        
-                    options={{
-                        fullScreen:{
-                            enable:true,
-                            zIndex:-1
-                        },
-                        background: {
-                            color: {
-                                value: "#3D405B",
-                            },
-                        },
-                        fpsLimit: 120,
-                        interactivity: {
-                            events: {
-                                onClick: {
-                                    enable: false,
-                                    mode: "push",
-                                },
-                                onHover: {
-                                    enable: true,
-                                    mode: "repulse",
-                                },
-                                resize: true,
-                            },
-                            modes: {
-                                push: {
-                                    quantity: 4,
-                                },
-                                repulse: {
-                                    distance: 200,
-                                    duration: 0.4,
-                                },
-                            },
-                        },
-                        particles: {
-                            color: {
-                                value: "#000",
-                            },
-                            links: {
-                                color: "#000",
-                                distance: 150,
-                                enable: true,
-                                opacity: 0.5,
-                                width: 1,
-                            },
-                            collisions: {
-                                enable: false,
-                            },
-                            move: {
-                                direction: "none",
-                                enable: true,
-                                outModes: {
-                                    default: "bounce",
-                                },
-                                random: false,
-                                speed: 2,
-                                straight: false,
-                            },
-                            number: {
-                                density: {
-                                    enable: true,
-                                    area: 800,
-                                },
-                                value: 80,
-                            },
-                            opacity: {
-                                value: 0.5,
-                            },
-                            shape: {
-                                type: "circle",
-                            },
-                            size: {
-                                value: { min: 1, max: 5 },
-                            },
-                        },
-                        detectRetina: true,
-                    }}
-                /> 
-                :
-                <Particles
-                    id="tsparticles"
-                    init={particlesInit}
-                    loaded={particlesLoaded}
-        
-                    options={{
-                        fullScreen:{
-                            enable:true,
-                            zIndex:-1
-                        },
-                        background: {
-                            color: {
-                                value: "#F4F1DE",
-                            },
-                        },
-                        fpsLimit: 120,
-                        interactivity: {
-                            events: {
-                                onClick: {
-                                    enable: false,
-                                    mode: "push",
-                                },
-                                onHover: {
-                                    enable: true,
-                                    mode: "repulse",
-                                },
-                                resize: true,
-                            },
-                            modes: {
-                                push: {
-                                    quantity: 4,
-                                },
-                                repulse: {
-                                    distance: 200,
-                                    duration: 0.4,
-                                },
-                            },
-                        },
-                        particles: {
-                            color: {
-                                value: "#000",
-                            },
-                            links: {
-                                color: "#000",
-                                distance: 150,
-                                enable: true,
-                                opacity: 0.5,
-                                width: 1,
-                            },
-                            collisions: {
-                                enable: false,
-                            },
-                            move: {
-                                direction: "none",
-                                enable: true,
-                                outModes: {
-                                    default: "bounce",
-                                },
-                                random: false,
-                                speed: 2,
-                                straight: false,
-                            },
-                            number: {
-                                density: {
-                                    enable: true,
-                                    area: 800,
-                                },
-                                value: 80,
-                            },
-                            opacity: {
-                                value: 0.5,
-                            },
-                            shape: {
-                                type: "circle",
-                            },
-                            size: {
-                                value: { min: 1, max: 5 },
-                            },
-                        },
-                        detectRetina: true,
-                    }}
-                />
-            )
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+    // This initializes the tsParticles instance (engine) here
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      console.log(container);
+    },
+    []
+  );
+
+  return toggle ? (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: {
+          enable: true,
+          zIndex: -1,
+        },
+        background: {
+          color: {
+            value: "#3D405B",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#000",
+          },
+          links: {
+            color: "#000",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: false,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  ) : (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: {
+          enable: true,
+          zIndex: -1,
+        },
+        background: {
+          color: {
+            value: "#F4F1DE",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#000",
+          },
+          links: {
+            color: "#000",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: false,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
 };
 
-export default Particle
-
+export default Particle;
